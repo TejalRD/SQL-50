34bb5f44f5f6a9c2f25a3b106f8206067de5ff05
Tejal Daga <90956881+TejalRD@users.noreply.github.com>
2025-11-30 15:42:22 -0500

    Time: 344 ms (39.47%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -4,3 +4,14 @@ LEFT join Employee e2
 ON e1.id = e2.managerId
 GROUP BY e1.Id,e1.name
 HAVING COUNT(e2.managerId)>=5
+
+#join condition-> Give me all pairs where the manager’s id = employee’s managerId
+-- order of execution:
+-- 1. FROM
+-- 2. JOIN
+-- 3. WHERE
+-- 4. GROUP BY
+-- 5. HAVING
+-- 6. SELECT
+-- 7. ORDER BY
+-- 8. LIMIT 
